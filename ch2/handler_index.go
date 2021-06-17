9778076c3cfc14f7b76ed4e57a5b6eba5e70a532
cat2koban <cat2koban@users.noreply.github.com>
2021-06-17 19:37:50 +0900

    add ch2/2-11

--- ch2/handler_index.go
+++ ch2/handler_index.go
@@ -1,18 +1,8 @@
 func index(w http.ResponseWriter, r *http.Request) {
   threads, err := data.Threads(); if err == nil {
-    _, err := session(w, r)
-    public_tmpl_files := []string{"templates/layout.html",
-                                  "templates/public.navbar.html",
-                                  "templates/index.html"}
-    private_tmpl_files := []string{"templates/layout.html",
-                                  "templates/public.navbar.html",
-                                  "templates/index.html"}
-    var templates *template.Template
-    if err != nil {
-      templates = template.Must(template.ParseFiles(private_tmpl_files...))
-    } else {
-      templates = template.Must(template.ParseFiles(public_tmpl_files...))
-    }
-    templates.ExecuteTemplate(w, "layout", threads)
+    _, err := session(writer, request)
+    generateHTML(writer, threads, "layout", "public.navbar", "index")
+  } else {
+    generateHTML(writer, threads, "layout", "private.navbar", "index")
   }
 }
